5c31c98a92554e33bc889789ef469957e0253b1f
Afn377 <afnanthegamer2018@gmail.com>
2024-11-05 14:41:05 -0500

    lecture on 11/5/24

---
+++ Class Coding/ColorPractice.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class ColorPractice{
+    public static void man(String[] args){
+        Color c1 = new Color(255, 0, 0);
+        StdDraw.setPenColor(c1);
+        StdDraw.filledCircle(0.5, 0.5, 0.3);
+    
+    }
+}
