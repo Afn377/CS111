5ba1136938c8b76b413a9db8e7ffe7c87ae1f263
Afn377 <afnanthegamer2018@gmail.com>
2024-11-05 11:51:36 -0500

    Finished QuadraticKoch.java

--- Assignment6/QuadraticKoch.java
+++ Assignment6/QuadraticKoch.java
@@ -5,6 +5,7 @@
  * @author Jeremy Hui
  *
  *************************************************************************/
+import java.awt.Color;
 public class QuadraticKoch {
 
     /**
@@ -22,6 +23,24 @@ public class QuadraticKoch {
      */
     public static double[][] getCoords(double x0, double y0, double x5, double y5) {
         // WRITE YOUR CODE HERE
+        if(x5 == x0){
+            double d = y5-y0;
+            double[][] coords = {
+                {x0, x0, x0-d/3, x0-d/3, x0, x5},
+                {y0, y0+d/3, y0+d/3, y0+2*d/3, y0+2*d/3, y5},
+            };
+            return coords;
+        }
+        else{
+            double d = x5-x0;
+            double[][] coords = {
+                {x0, x0+d/3, x0+d/3, x0+2*d/3, x0+2*d/3, x5},
+                {y0, y0, y0+d/3, y0+d/3, y0, y5},
+            };
+            
+            return coords;
+        }
+        
     }
 
     /**
@@ -36,8 +55,22 @@ public class QuadraticKoch {
      * @param n  The current order
      */
     public static void koch(double x0, double y0, double x5, double y5, int n) {
-        // WRITE YOUR CODE HERE
+        StdDraw.setPenColor(Color.BLACK);
+        if (n == 0) {
+            StdDraw.line(x0, y0, x5, y5);
+            return;
+        }
+        double[][] coords = getCoords(x0, y0, x5, y5);
+    
+        koch(x0, y0, coords[0][1], coords[1][1], n - 1);  // First segment
+        koch(coords[0][1], coords[1][1], coords[0][2], coords[1][2], n - 1);  // Second segment (spike up)
+        koch(coords[0][2], coords[1][2], coords[0][3], coords[1][3], n - 1);  // Third segment (spike down)
+        koch(coords[0][3], coords[1][3], coords[0][4], coords[1][4], n - 1);  // Fourth segment
+        koch(coords[0][4], coords[1][4], x5, y5, n - 1);  // Last segment to (x5, y5)
+        return;
+
     }
+    
 
     /**
      * Takes an integer command-line argument n,
@@ -48,5 +81,12 @@ public class QuadraticKoch {
      */
     public static void main(String[] args) {
         // WRITE YOUR CODE HERE
+        StdDraw.setCanvasSize(800, 800);
+        int n = Integer.parseInt(args[0]);
+        koch(0.25, 0.25, 0.25, 0.75, n);
+        koch(0.25, 0.75, 0.75, 0.75, n);
+        koch(0.75, 0.75, 0.75, 0.25, n);
+        koch(0.75, 0.25, 0.25, 0.25, n);
+
     }
 }
