b011f647fc859d9f62b94d2e8fc688dbad7049b7
Afn377 <afnanthegamer2018@gmail.com>
2024-11-13 20:07:44 -0500

    removed replicated assignment 1

--- Assignment1/GravitationalForce.java
+++
@@ -1,9 +0,0 @@
-public class GravitationalForce{
-    public static void main(String[] args) {
-        double m1 = Double.parseDouble(args[0]);
-        double m2 = Double.parseDouble(args[1]);
-        double r = Double.parseDouble(args[2]);
-        double F = 6.6743e-11 * (m1*m2) / (r*r);
-        System.out.println(F);
-    }
-}
--- Assignment2/Palindrome.java
+++ Assignment2/Palindrome.java
@@ -1,28 +1,11 @@
-/*
- * Write the Palindrome program inside the main method
- * according to the assignment description.
- * 
- * To compile:
- *        javac Palindrome.java
- * To execute:
- *        java Palindrome 5 4 6 6 4 5
- * 
- * DO NOT change the class name
- * DO NOT use System.exit()
- * DO NOT change add import statements
- * DO NOT add project statement
- * 
- */
-
-public class Palindrome {
+public class Palindrome{
     public static void main(String[] args) {
-       
         int i1 = Integer.parseInt(args[0]);
-        int i2 = Integer.parseInt(args[1]);
-        int i3 = Integer.parseInt(args[2]);
-        int i4 = Integer.parseInt(args[3]);
-        int i5 = Integer.parseInt(args[4]);
-        int i6 = Integer.parseInt(args[5]);
+        int i2 = Integer.parseInt(args[0]);
+        int i3 = Integer.parseInt(args[0]);
+        int i4 = Integer.parseInt(args[0]);
+        int i5 = Integer.parseInt(args[0]);
+        int i6 = Integer.parseInt(args[0]);
         System.out.println((i1 == i6 && i2 == i5 && i3 == i4));
-    }
+}
 }
