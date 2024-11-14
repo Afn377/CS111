c4bdee2c0a08b2ca68e1e05220d62cf2ace75a15
Afn377 <afnanthegamer2018@gmail.com>
2024-11-13 20:02:05 -0500

    made a folder for assignment1, and compiled all assignment 1 files

---
+++ Assignment1/Palindrome.java
@@ -0,0 +1,11 @@
+public class Palindrome{
+    public static void main(String[] args) {
+        int i1 = Integer.parseInt(args[0]);
+        int i2 = Integer.parseInt(args[0]);
+        int i3 = Integer.parseInt(args[0]);
+        int i4 = Integer.parseInt(args[0]);
+        int i5 = Integer.parseInt(args[0]);
+        int i6 = Integer.parseInt(args[0]);
+        System.out.println((i1 == i6 && i2 == i5 && i3 == i4));
+}
+}
